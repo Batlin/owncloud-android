55c39aceea50c2b622f76b6ff76b4af41dd23f90
Hannes Achleitner <hannes.software@gmx.at>
2018-12-19 08:08:12 +0100

    AndroidX

--- owncloudApp/src/main/java/com/owncloud/android/services/OperationsService.java
+++ owncloudApp/src/main/java/com/owncloud/android/services/OperationsService.java
@@ -36,6 +36,7 @@ import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
 import android.os.Process;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import android.util.Pair;
 
 import com.owncloud.android.MainApp;
